0d67ae31e394c965d8f6e798326c1f649b4776fa
Pavan Balakrishna <pavan.hbalakrishna@gmail.com>
2021-04-21 14:34:13 +0900

    removed config

--- Python/Practise/MyFile.py
+++ Python/Practise/MyFile.py
@@ -1,3 +1,15 @@
 age=float(30.442)
+multilinestr = '''hello there
+my name is pavan and
+this is
+a
+multiline text'''
+print(multilinestr)
 
-print(age)
+
+a = "Hello, World!"
+print(a[0])
+
+
+for x in 'vanasdad':
+    print(x)
